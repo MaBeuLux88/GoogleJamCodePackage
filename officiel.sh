a8947a1a661abf75d4d229ea1691759cc9c432aa
Maxime Beugnet <maxime.beugnet@gmail.com>
2015-04-11 19:54:22 +0200

    avoid error when first official run

--- officiel.sh
+++ officiel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p save_problem/old_problems
-mv -f save_problem/*in save_problem/old_problems/
+mv -f save_problem/*in save_problem/old_problems/ 2> /dev/null
 mv ~/Downloads/*in save_problem/
-cp save_problem/*in problem.in
+cp save_problem/*in problem.in 
 mvn exec:java -Dexec.mainClass="com.google.Main"
 ./archive.sh
