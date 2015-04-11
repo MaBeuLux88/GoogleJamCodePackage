24cca983f37d29e60070fa10e4aa13de560c12dc
Maxime Beugnet <maxime.beugnet@gmail.com>
2015-04-11 22:46:32 +0200

    updated common unit test

--- src/test/java/com/google/ProblemTest.java
+++ src/test/java/com/google/ProblemTest.java
@@ -1,11 +1,14 @@
 package com.google;
 
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.Test;
 
-public class Test {
+public class ProblemTest {
 
-    @org.testng.annotations.Test
+    @Test
     public void testName() throws Exception {
+        ProblemSample pb = new ProblemSample();
+        pb.solve();
         Assertions.assertThat(true).isTrue();
     }
 
